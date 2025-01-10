1eae89b8665066917930285a8435fe4ba898b8e4
Alec Breton <acbreton@github.com>
2025-01-10 16:22:09 -0500

    Update hide logic on non-Google sites

--- contentScript.js
+++ contentScript.js
@@ -1,81 +1,99 @@
-let isEnabled;
-let html = document.getElementsByTagName("html")[0];
-let placeholderDiv = document.createElement("div");
-placeholderDiv.setAttribute("id", "placeholder");
+const blockStyleElement = document.createElement('style');
+const isProdMode = 'update_url' in chrome.runtime.getManifest()
 
-function createLoadingPlaceholder() {
-    let newContent = document.createTextNode("Removing User and Critic Ratings...");
+const SITE_DETECTION = {
+    imdb: /imdb\.com/,
+    goodreads: /goodreads\.com/,
+};
 
-    placeholderDiv.style.height = "100%";
-    placeholderDiv.style.width = "100%";
-    placeholderDiv.style.position = "absolute";
-    placeholderDiv.style.zIndex = "100";
-    placeholderDiv.style.backgroundColor = "black";
-    placeholderDiv.style.display = "flex";
-    placeholderDiv.style.alignItems = "center";
-    placeholderDiv.style.justifyContent = "center";
-    placeholderDiv.style.color = "white";
-
-    html.style.overflowY = 'hidden';
-
-    placeholderDiv.appendChild(newContent);
-    
-    html.appendChild(placeholderDiv);
-
-    // In case it doesn't remove on it's own
-    setTimeout(() => removePlaceholder(), 7000);
+const CLASSNAMES_TO_REMOVE = {
+    goodreads: [
+        '[class*="AverageRating"]',
+        '[class*="BookPageMetadataSection__ratingStats"]',
+        '[class*="ReviewContent"]',
+        '[id*="ReviewsSection"]',
+    ],
+    imdb: [
+        '[class^="RatingBar"]',
+        '[class*="rating"]',
+        '[class*="ipc-reaction-summary"]',
+        '[data-testid="awards"]',
+        '[data-testid*="hero-rating-bar"]',
+        '[data-testid*="reviewContent"]',
+        '[data-testid="UserReviews"]',
+    ],
 }
 
-function removePlaceholder() {
-    let elem = document.getElementById('placeholder');
-    elem && elem.parentNode.removeChild(elem);
-    html.style.overflowY = 'inherit';
-}
-
-function init() {
-    createLoadingPlaceholder();
-
-    window.addEventListener('load', function() {
-        chrome.storage.local.get("enabled", (result) => {
-            isEnabled = result.enabled !== undefined ? result.enabled : true;
-            
-            if (isEnabled) {
-                runScripts();
-            }
-        });
-
-        chrome.storage.onChanged.addListener((changes) => {
-            changes.enabled
-                ? runScripts(changes.enabled.newValue)
-                : runScripts(isEnabled);
-        });
-    }, false);
-}
-
-function runScripts(extensionEnabled = true) {
-    if (window.location.host.includes("imdb")) {
-        const classNamesToRemove = [
-            '[class^="RatingBar"]',
-            '[class*="rating"]',
-            '[class*="ReviewContent"]',
-            '[data-testid="awards"]',
-            '[data-testid*="hero-rating-bar"]',
-            '[data-testid*="reviewContent"]',
-            '[data-testid="UserReviews"]',
-        ];
-
-        for (let className of classNamesToRemove) {
-            let elems = document.querySelectorAll(className);
-
-            if (elems.length) {
-                for (elem of elems) {
-                    elem.style.display = extensionEnabled ? "none" : "block";
-                }
-            }
+function getSiteKey() {
+    for (const [key, regex] of Object.entries(SITE_DETECTION)) {
+        if (regex.test(window.location.host)) {
+            return key;
         }
+    }
+    return null;
+}
 
-        removePlaceholder();
+
+function log(message) {
+    if (!isProdMode) {
+        console.log(message);
     }
 }
 
-init(html);
+function hideElementsForSite() {
+    if (!document.head) {
+        log('Head not available yet, waiting...');
+        return;
+    }
+
+    const site = getSiteKey();
+    if (!site) return;
+
+    blockStyleElement.textContent = `${CLASSNAMES_TO_REMOVE[site].join(', ')} { display: none !important; }`;
+
+    log(`Injected CSS to hide elements with class: ${CLASSNAMES_TO_REMOVE[site]}`);
+
+    document.head.appendChild(blockStyleElement);
+}
+
+function showElementsForSite() {
+    blockStyleElement.remove();
+}
+
+function toggleElementsForSite(isEnabled) {
+    if (isEnabled) {
+        hideElementsForSite();
+    } else {
+        showElementsForSite();
+    }
+}
+
+function observeHead() {
+    const observer = new MutationObserver(() => {
+        if (document.head) {
+            hideElementsForSite();
+            observer.disconnect();
+        }
+    });
+
+    // Start observing the document for added elements in the <html> tag
+    observer.observe(document.documentElement, {
+        childList: true, // Look for added child nodes
+        subtree: false // Observe all descendants
+    });
+
+}
+
+function init() {
+    chrome.storage.local.get("enabled", (result) => {
+        if (result.enabled) observeHead();
+    });
+
+    chrome.storage.onChanged.addListener((changes) => {
+        if ('enabled' in changes) {
+            toggleElementsForSite(changes.enabled.newValue);
+        }
+    });
+}
+
+init();
