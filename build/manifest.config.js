40551f06320566999ee24dcb0eb1b798b7bd5f2a
Alec Breton <acbreton@github.com>
2026-08-14 17:04:43 -0400

    Declare no data collection for Firefox (AMO requirement)

--- build/manifest.config.js
+++ build/manifest.config.js
@@ -75,7 +75,10 @@ const targets = {
         background: { scripts: ["background.js"], persistent: false },
         extra: {
             browser_specific_settings: {
-                gecko: { id: "{1e883c16-fc94-4cf8-b58f-f34b445f632e}" },
+                gecko: {
+                    id: "{1e883c16-fc94-4cf8-b58f-f34b445f632e}",
+                    data_collection_permissions: { required: ["none"] },
+                },
             },
         },
     },
